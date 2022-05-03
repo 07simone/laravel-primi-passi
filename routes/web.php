72d4a87e2aa0e0842a9d19fbb10401d8c0b8f059
simone andreozzi <simoneandreozzi8@gmail.com>
2022-05-03 16:16:48 +0200

    esercizio-complex-più-bonus

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/contatti', function () {
+    return view('contacts');
+});
+
